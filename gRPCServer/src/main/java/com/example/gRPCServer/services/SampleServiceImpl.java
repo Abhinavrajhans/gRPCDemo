6fd90817728e4d6efab655bee2f051e6eb3438c2
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-12-06 13:56:37 +0530

    first commit

---
+++ gRPCServer/src/main/java/com/example/gRPCServer/services/SampleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.gRPCServer.services;
+import com.example.gRPCServer.SampleRequest;
+import com.example.gRPCServer.SampleResponse;
+import com.example.gRPCServer.SampleServiceGrpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
+
+    @Override
+    public void saySampleHello(SampleRequest request, StreamObserver<SampleResponse> responseObserver) {
+        String name = request.getName();
+        String message = "Hello, " + name + "! Welcome to the sample gRPC service.";
+
+        SampleResponse response = SampleResponse.newBuilder().setMessage(message).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
